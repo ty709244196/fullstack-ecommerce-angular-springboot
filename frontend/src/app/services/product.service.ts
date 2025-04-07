b50650c3478c45b1f367308a194001dc60c9a5f9
Yao Tang <kevintang35@gmail.com>
2025-04-06 23:50:54 -0500

    Impl search bar -search by keyword

--- frontend/src/app/services/product.service.ts
+++ frontend/src/app/services/product.service.ts
@@ -9,7 +9,7 @@ import { ProductCategory } from '../common/product-category';
   providedIn: 'root'
 })
 export class ProductService {
-  
+
   private baseUrl = 'http://localhost:8080/api/products'
   private categoryURL = 'http://localhost:8080/api/product-category'
 
@@ -20,38 +20,50 @@ export class ProductService {
     //build URL based on category id
     const searchURL = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}`;
 
-    return this.httpClient.get<GetResponseProducts>(searchURL).pipe(
-      map(response => response._embedded.products)
-    )
+    return this.getProducts(searchURL)
+
+  }
+
+  searchProducts(theKeyWord: string): Observable<Product[]> {
+    const searchURL = `${this.baseUrl}/search/findByNameContaining?name=${theKeyWord}`;
+
+    return this.getProducts(searchURL)
+
   }
 
 
-//   testCORS() {
-//     const headers = new HttpHeaders().set('X-Test-Header', 'value');
-//     this.httpClient.post(this.baseUrl, { name: 'Test' }, { headers })
-//       .subscribe(
-//         response => console.log('Response:', response),
-//         error => console.error('CORS Error:', error)
-//       );
-  
-// }
+  private getProducts(searchURL: string): Observable<Product[]> {
+    return this.httpClient.get<GetResponseProducts>(searchURL).pipe(
+      map(response => response._embedded.products)
+    );
+  }
 
-getProductCategories(): Observable<ProductCategory[]> {
-  return this.httpClient.get<GetResponseProductsCategory>(this.categoryURL).pipe(
-    map(response => response._embedded.productCategory)
-  )  
-}
+  //   testCORS() {
+  //     const headers = new HttpHeaders().set('X-Test-Header', 'value');
+  //     this.httpClient.post(this.baseUrl, { name: 'Test' }, { headers })
+  //       .subscribe(
+  //         response => console.log('Response:', response),
+  //         error => console.error('CORS Error:', error)
+  //       );
+
+  // }
+
+  getProductCategories(): Observable<ProductCategory[]> {
+    return this.httpClient.get<GetResponseProductsCategory>(this.categoryURL).pipe(
+      map(response => response._embedded.productCategory)
+    );
+  }
 }
 
-interface GetResponseProducts{
+interface GetResponseProducts {
   _embedded: {
     products: Product[];
   }
 }
 
-  interface GetResponseProductsCategory{
-    _embedded: {
-      productCategory: ProductCategory[];
-    }
+interface GetResponseProductsCategory {
+  _embedded: {
+    productCategory: ProductCategory[];
+  }
 }
 
